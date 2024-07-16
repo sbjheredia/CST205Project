f16c927600cded17191986fa64a43edc3a01567e
katesawtell <katesawtell99@gmail.com>
2024-07-15 17:19:27 -0700

    can upload 1 image and show it on gallery page

--- CST205_project.py
+++ CST205_project.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap5
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask_bootstrap import Bootstrap
 from random import shuffle
 from PIL import Image
 import os
@@ -8,8 +8,15 @@ import os
 #boom test again
 
 app = Flask(__name__)
-bootstrap = Bootstrap5(app)
+bootstrap = Bootstrap(app)
 
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+#images will go in this list
 gallery_images = []
 
 @app.route('/')
@@ -21,7 +28,33 @@ def home():
         So we'd have the code to handle the forms and uploading of
         the images here. maybe idkslacj
     '''
+    gallery_images = []
     return render_template('home.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_files():
+    if 'files' not in request.files:
+        return redirect(request.url)
+    
+    files = request.files.getlist('files')
+    
+    if not files or files[0].filename == '':
+        return redirect(request.url)
+    
+    for file in files:
+        if file:
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filepath)
+            gallery_images.append(GalleryImage(filepath, "example title", Image.open(filepath)))
+
+
+    
+    return render_template('gallery.html', files=gallery_images)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 @app.route('/gallery')
@@ -37,9 +70,14 @@ class GalleryImage:
         image is a Pillow Image object
     '''
     # If we want to add more details, they'll go here
-    def __init__(self, image, title, description, filter, frame):
+    # def __init__(self, image, title, description, filter, frame):
+    #     self.image = image
+    #     self.title = title
+    #     self.description = description
+    #     self.filter = filter
+    #     self.frame = frame
+
+    def __init__(self, source, title, image):
         self.image = image
+        self.source = source
         self.title = title
-        self.description = description
-        self.filter = filter
-        self.frame = frame
